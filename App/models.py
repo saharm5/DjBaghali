910bf1c8b4383b0714fc9c6c2adf096215aaeaf7
sahar mohammadi <saharmohammadi0224@gmail.com>
2025-02-09 10:15:10 +0330

    DOSENT WORKING

---
+++ App/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    product_name = models.CharField(max_length=255)
+    product_details = models.TextField()
+    main_price = models.IntegerField()
+    Discount = models.IntegerField(null=True, blank=True)
+    final_price = models.IntegerField()
+    brand = models.CharField(max_length=255)
+    brand_image_src = models.URLField()
+    category = models.CharField(max_length=255)
+    sub_category = models.CharField(max_length=255)
+    category_image_src = models.URLField()
+    size = models.CharField(max_length=50)
+    production_date = models.CharField(max_length=50, null=True, blank=True)
+    expiration_date = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.product_name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name="images", on_delete=models.CASCADE)
+    productImageSrc = models.URLField()
+
+    def __str__(self):
+        return f"Image for {self.product.product_name}"
