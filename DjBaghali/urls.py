bc18b517f1b6a8428250508bebbb1a31f907708f
sahar mohammadi <saharmohammadi0224@gmail.com>
2025-02-09 10:25:48 +0330

    SAJFHCSD

--- DjBaghali/urls.py
+++ DjBaghali/urls.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from authentication.views import AuthView
-from App.views import data_products, post_favorit_request, post_review_request
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
-    path('login/', AuthView.as_view(), name='login'),
-    path("auth/", AuthView.as_view(), name="auth"),
-    path('data/', data_products),
-    path('save-data/', post_favorit_request),
-    path('regmsg/', post_review_request),
-
 ]
--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
 from .views import AuthView
 
 urlpatterns = [
     path('login/', AuthView.as_view(), name='login'),
     path("auth/", AuthView.as_view(), name="auth"),
+
 ]
